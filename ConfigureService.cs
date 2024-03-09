13300ae3484dc0300a6d47dfb6a3321f7d2176f3
abdollahsafari <abdollahsafari1986@gmail.com>
2024-03-09 17:07:47 +0330

    Create ConfigureService.cs

---
+++ ConfigureService.cs
@@ -0,0 +1,14 @@
+﻿using TeachingWebSite.Contracts;
+using TeachingWebSite.Models;
+
+namespace TeachingWebSite;
+
+public static class ConfigureService
+{
+    public static IServiceCollection RegisterPresentationServices(this IServiceCollection services)
+    {
+        //services.AddScoped<ITeacherBusiness, Teacher>()
+
+        return services;
+    }
+}
