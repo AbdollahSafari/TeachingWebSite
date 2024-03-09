b03d7148250610b6041d3f30154e7c4622025e99
abdollahsafari <abdollahsafari1986@gmail.com>
2024-03-09 17:07:50 +0330

    Create TeacherController.cs

---
+++ Controllers/V1/TeacherController.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Options;
+using TeachingWebSite.Contracts;
+using TeachingWebSite.Shared.Configs;
+
+namespace TeachingWebSite.Controllers.V1;
+
+
+public class TeacherController : BaseController
+{
+    private readonly ITeacherBusiness _teacherBusiness;
+    private readonly IMapper _mapper;
+    private readonly MySettings _mySettings;
+
+    public TeacherController(ITeacherBusiness teacherBusiness, IMapper mapper, IOptionsSnapshot<MySettings> mySettings)
+    {
+        _mapper = mapper;
+        _mySettings = mySettings.Value;
+        _teacherBusiness = teacherBusiness;
+    }
+}
