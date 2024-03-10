6a29e65fe1e02840181a3f81bd9b4b035b636d17
abdollahsafari <abdollahsafari1986@gmail.com>
2024-03-10 15:54:17 +0330

    Create TeacherMapperProfile.cs

---
+++ MapperProfiles/TeacherMapperProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace TeachingWebSite.MapperProfiles;
+
+public class TeacherMapperProfile : Profile
+{
+    public TeacherMapperProfile()
+    {
+        //Source,Destination
+        //CreateMap<>()
+    }
+}
