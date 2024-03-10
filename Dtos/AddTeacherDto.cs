1265812240b32a5af6c78f9203405fd56e306ade
abdollahsafari <abdollahsafari1986@gmail.com>
2024-03-10 15:54:12 +0330

    Create AddTeacherDto.cs

---
+++ Dtos/AddTeacherDto.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using TeachingWebSite.Contracts;
+
+namespace TeachingWebSite.Dtos;
+
+public class AddTeacherDto
+{
+    public Name Name { get; set; }
+    public int ClassPricePerHour { get; set; }
+}
+
+public class AddProductDtoValidator : AbstractValidator<AddTeacherDto>
+{
+    public AddProductDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("Please enter valid name!");
+    }
+}
