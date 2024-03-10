42ed9dc9ed04c6f1e7556df5fa854592b6fbdb99
abdollahsafari <abdollahsafari1986@gmail.com>
2024-03-10 15:53:57 +0330

    Create TeacherBusiness.cs

---
+++ Business/TeacherBusiness.cs
@@ -0,0 +1,49 @@
+﻿using TeachingWebSite.Contracts;
+using TeachingWebSite.Models;
+
+namespace TeachingWebSite.Business
+{
+    public class TeacherBusiness : ITeacherBusiness
+    {
+        static public List<Teacher> Teachers = new List<Teacher>()
+        {
+            new Teacher()
+            {
+                Id = 1, ClassPricePerHour = 10, CreateDateTime = DateTime.Now, IsActive = true,
+                TeacherName = new Name(){FirstName = "first1", LastName = "last1"}, 
+            },
+            new Teacher()
+            {
+                Id = 1, ClassPricePerHour = 10, CreateDateTime = DateTime.Now, IsActive = true,
+                TeacherName = new Name(){FirstName = "first2", LastName = "last2"},
+            },
+            new Teacher()
+            {
+                Id = 1, ClassPricePerHour = 10, CreateDateTime = DateTime.Now, IsActive = true,
+                TeacherName = new Name(){FirstName = "first3", LastName = "last3"},
+            },
+            new Teacher()
+            {
+                Id = 1, ClassPricePerHour = 10, CreateDateTime = DateTime.Now, IsActive = true,
+                TeacherName = new Name(){FirstName = "first4", LastName = "last4"},
+            },
+        };
+
+        public List<Teacher> GetTeacher()
+        {
+            return Teachers;
+        }
+
+        public Teacher GetTeacherById(int id)
+        {
+            var teacher = Teachers.FirstOrDefault(x => x.Id == id);
+            return teacher;
+        }
+
+        public bool AddTeacher(Teacher teacher)
+        {
+            Teachers.Add(teacher);
+            return true;
+        }
+    }
+}
