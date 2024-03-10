57ee6a619c61560a466056b461251c5fcb101e03
abdollahsafari <abdollahsafari1986@gmail.com>
2024-03-10 15:53:59 +0330

    Create TeacherBusiness2.cs

---
+++ Business/TeacherBusiness2.cs
@@ -0,0 +1,23 @@
+﻿using TeachingWebSite.Contracts;
+using TeachingWebSite.Models;
+
+namespace TeachingWebSite.Business
+{
+    public class TeacherBusiness2 : ITeacherBusiness
+    {
+        public List<Teacher> GetTeacher()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Teacher GetTeacherById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AddTeacher(Teacher teacher)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
